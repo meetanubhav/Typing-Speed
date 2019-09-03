a42c6bd4d96975d337181dc0867ab356d9345ec5
Anubhav Gupta <37771611+meetanubhav@users.noreply.github.com>
2019-09-03 16:05:01 +0530

    Update main.py
    Major changes :
    * Changed import time
    * Change in recursive input function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from time import time
+import time
 import random
 import msvcrt
-
+Start_time=0
 def TestSentence():
     s1= "This is america. "
     s2="Big fox ate a little dog."
@@ -14,6 +14,7 @@ def TestSentence():
 def getString():
     getText=TestSentence()
     CharString=list()
+    print(getText)
     # putting string into list
     for getText in getText:
         CharString.append(getText)
@@ -26,10 +27,14 @@ def getUserString(CharString,StringLen):
     print('Begin :')
     error=0
     correct=0
+    global Start_time
+    Start_time=time.time()
+    # print(CharString)
     for i in range (0,StringLen):
         # getuserInput=raw_input()
-        print(CharString[i])
+        # print(CharString[i])
         getuserInput = msvcrt.getch()
+        print(getuserInput)
         check=calculate(CharString,getuserInput,i)
         if(check==1):
             correct=correct+1
@@ -39,13 +44,15 @@ def getUserString(CharString,StringLen):
 
 def calculate(CharString,getuserInput,i):
     if(ord(getuserInput)==ord(CharString[i])):
-            return(1)
+    	return(1)
     else:
         return(0)
 
 def result(correctCount,errorCount):
     print('Correct Count : \t',correctCount)
     print('\nError Count : \t',errorCount)
+    tm=(time.time()-Start_time)/10
+    print('Time taken =',"%.3f" %tm,' ms')
 if __name__ == "__main__":
     # Clear screen(cmd/terminal)
     # _ = call('clear' if os.name =='posix' else 'cls')
